81b5912c1960970232604becb3a7e4a7bc461792
mufaqar <mufaqar@gmail.com>
2025-03-28 15:48:27 +0500

    updated

--- src/templates/product/related-products.tsx
+++ src/templates/product/related-products.tsx
@@ -8,7 +8,7 @@ const RelatedProducts = async () => {
   });
   return (
     <div className="bg-primary">
-      <h2 className="text-5xl md:text-6xl lg:text-7xl xl:text-9xl font-cervo py-10 px-3 text-dark uppercase text-center">
+      <h2 className="text-5xl md:text-6xl lg:text-7xl xl:text-9xl font-cervo py-10 px-3 text-dark uppercase text-center ">
         Worth a try
       </h2>
       <div className="grid grid-cols-1 max-w-[1480px] mx-auto px-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 pb-10">
