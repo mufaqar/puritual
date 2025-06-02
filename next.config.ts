36f17858da1703ab190f5ad9cc31a620b217b76e
Mufaqar <mufaqar@gmail.com>
2025-06-02 15:07:19 +0500

    updated

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   },
   images: {
     domains: [
-      "localhost", "demo.mufaqar.com"
+      "localhost", "moccasin-chinchilla-599653.hostingersite.com"
     ],
   },
 };
