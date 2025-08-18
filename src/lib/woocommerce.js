397a564f73e0f3717626cbdaa1f2afa39a644215
Mufaqar <mufaqar@gmail.com>
2025-08-18 20:07:06 +0500

    woo

--- src/lib/woocommerce.js
+++ src/lib/woocommerce.js
@@ -1,15 +1,11 @@
 import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
 
-import https from "https";
-
 const WooCommerce = new WooCommerceRestApi({
   url: process.env.NEXT_PUBLIC_WC_URL,
   consumerKey: process.env.NEXT_PUBLIC_WC_CONSUMER_KEY,
   consumerSecret: process.env.NEXT_PUBLIC_WC_CONSUMER_SECRET,
   version: process.env.NEXT_PUBLIC_WC_VERSION,
-   axiosConfig: {
-    httpsAgent: new https.Agent({ rejectUnauthorized: false }) // 👈 disables SSL verification
-  }
+  
 });
 
 export default WooCommerce;
