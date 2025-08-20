719f6064800e91f4328fb77f51a1beecb93cff83
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2025-08-20 19:10:43 +0500

    Container

--- src/templates/Premium.tsx
+++ src/templates/Premium.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 function Premium() {
   return (
     <div className="w-full h-screen md:py-16 py-5 md:bg-[url('/images/Premium.png')] bg-no-repeat bg-cover bg-center bg-[url('/images/premium_mbl.png')]">
-      <div className='flex flex-col-reverse md:flex-row md:gap-6 gap-0 justify-between'>
+      <div className='flex flex-col-reverse md:flex-row md:gap-6 gap-0 justify-between container mx-auto md:px-3 px-3'>
         {/* img - now on left side */}
-        <div className='w-full md:w-[60%] '>
+        <div className='w-full md:w-[65%] '>
           
         </div>
         {/* text content - now on left side */}
-        <div className='w-full md:w-[25%]'>
-          <div className='py-10 px-3 md:pl-10 lg:pl-10'>
+        <div className='w-full md:w-[35%]'>
+          <div className='py-10 md:pl-10 lg:pl-10'>
             <h2 className='font-normal md:text-[70px] md:leading-[70px] text-3xl text-dark'>
               Premium<br />
               <span className='font-medium md:text-[110px] md:leading-[90px] text-5xl'>FOAMING</span>
