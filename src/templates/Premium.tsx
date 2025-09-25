8a651498c063cb5365b5218b2fa4d990ae8ce469
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2025-09-25 18:02:07 +0500

    updated

--- src/templates/Premium.tsx
+++ src/templates/Premium.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Premium() {
   return (
-    <div className="w-full md:h-screen h-[550px] md:py-16 py-5 md:bg-[url('/images/Premium.png')] bg-no-repeat bg-cover bg-center bg-none relative">
+    <div className="w-full md:h-screen h-[550px] md:py-16 py-3 md:bg-[url('/images/Premium.png')] bg-no-repeat bg-cover bg-center bg-none relative">
       <Image src="/images/premium.webp" alt='premium' width={1667} height={1667} className='w-full h-full absolute top-0 md:hidden block' />
       <div className='flex flex-col-reverse md:flex-row md:gap-6 gap-0 justify-between container mx-auto md:px-3 px-3 relative z-[3]'>
         {/* img - now on left side */}
@@ -11,13 +11,13 @@ function Premium() {
         </div>
         {/* text content - now on left side */}
         <div className='w-full md:w-[35%]'>
-          <div className='py-5 md:pl-10 lg:pl-10'>
+          <div className='py-2 md:pl-10 lg:pl-10'>
             <h2 className='font-normal md:text-[70px] md:leading-[70px] text-3xl text-dark'>
               Premium<br />
               <span className='font-medium md:text-[110px] md:leading-[90px] text-5xl'>FOAMING</span>
             </h2>
             <h2 className='ml-10 md:ml-10 lg:ml-18 font-normal md:text-[70px] md:leading-[70px] text-3xl text-dark'>Hand Wash</h2>
-            <div className='ml-0 md:ml-10 lg:ml-18 pt-6 md:w-full w-[67%]'>
+            <div className='ml-0 md:ml-10 lg:ml-18 md:pt-6 pt-2 md:w-full w-[67%]'>
               <p className='text-sm md:text-2xl lg:text-3xl text-dark font-normal'>
                 <span className='md:font-normal font-bold'>No Gel</span>, Same clean, more fun!
               </p>
